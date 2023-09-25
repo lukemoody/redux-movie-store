50dcbea69fa8ca649fdd4d9f5d636e6eb82b2705
lukemoody <luke_moody@hotmail.com>
2023-09-25 23:31:25 +0100

    react nto working as expected when trying to use use client

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
+"use client";
+import { useSelector, useDispatch } from "react-redux";
+import { useState, useEffect } from "react";
+
+// Take STATE and send it into REDUX
+
 export default function Home() {
+  const [movieTitle, setMovieTitle] = useState<string>("");
+
+  useEffect(() => {
+    console.log("hello");
+    console.log("movieTitle", movieTitle);
+  }, [movieTitle, setMovieTitle]);
+
   return (
     <>
-      <h1>Testing 123</h1>
+      <h1>My Movie List</h1>
+      <input
+        type="text"
+        placeholder="Enter a movie title"
+        value={movieTitle}
+        onChange={(e) => setMovieTitle(e.target.value)}
+      />
+      {console.log("movieTitle", movieTitle)}
     </>
   );
 }
